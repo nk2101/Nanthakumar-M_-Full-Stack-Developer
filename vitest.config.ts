48c6ab000f5d8c34d688d8aa9d5ceef2a8f4873a
Nanthakumar <nanthaakumar36@gmail.com>
2026-06-11 12:45:59 +0530

    deploy changes add in repo

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: '/nanthakumar-full-stack-developer/',
+  base: '/Nanthakumar-M_-Full-Stack-Developer/',
   plugins: [react()],
   test: {
     environment: "jsdom",
